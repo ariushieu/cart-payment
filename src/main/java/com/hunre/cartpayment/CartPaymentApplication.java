c3b566f0e95f19c32a4f1d572dfb2070bf965fad
Hieu Nguyen <144506842+ariushieu@users.noreply.github.com>
2026-08-05 13:46:33 +0700

    chore: setup base project structure and empty templates

---
+++ src/main/java/com/hunre/cartpayment/CartPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.hunre.cartpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hunre/cartpayment/CartPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunre.cartpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
